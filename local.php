a270ea929d8bdf4f415ce54fa82c1fb08295fb8e
moxan1 <trace@moxan.top>
2025-05-25 17:34:12 +0800

    Update local.php

--- local.php
+++ local.php
@@ -1,5 +1,5 @@
 <?php
-$img_array = glob("img/*.{gif,jpg,png}" ,GLOB_BRACE);
+$img_array = glob("img/*.{gif,jpg,png,webp}" ,GLOB_BRACE);
 $img = array_rand($img_array);
 $dz = $img_array[$img];
 header("Location:".$dz);
